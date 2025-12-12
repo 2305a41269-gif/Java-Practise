84c404617eafa6769716b4efcfcc079de409460d
2305a41269-gif <2305a41269@sru.edu.in>
2025-12-12 12:23:53 +0530

    Add files via upload

---
+++ Stringbuild.java
@@ -0,0 +1,16 @@
+public class Stringbuild {
+    public static void main (String[]args){
+        StringBuilder sb=new StringBuilder("Hello ");
+        sb.append("World");
+
+        System.out.println(sb);
+    
+
+    }
+
+    
+}
+
+
+
+
